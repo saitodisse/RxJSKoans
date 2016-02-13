0b4c1a37e7b1abd81a45b53e2336367f25aef201
Julio Makdisse Saito <saitodisse@gmail.com>
2016-02-13 15:20:59 -0200

    02

--- test/02_about_composable_observations.js
+++ test/02_about_composable_observations.js
@@ -1,7 +1,7 @@
 var Rx = require('rx'),
-    Observable = Rx.Observable,
-    Subject = Rx.Subject,
-    Range = require('../util/range');
+  Observable = Rx.Observable,
+  Subject = Rx.Subject,
+  Range = require('../util/range');
 
 QUnit.module('Composable Observations');
 
@@ -9,34 +9,36 @@ var __ = 'Fill in the blank';
 
 test('composable add', function () {
   var received = 0,
-      numbers = [10, 100, __];
+    numbers = [10, 100, 1000];
 
-  Observable.from(numbers).sum().subscribe(function (x) { received = x; });
+  Observable.fromArray(numbers)
+    .sum()
+    .subscribe(function (x) { received = x; });
 
   equal(1110, received);
 });
 
 test('composable before and after', function () {
   var names = Range.create(1, 6),
-      a = '',
-      b = '';
+    a = '',
+    b = '';
 
-  Observable.from(names)
-    .tap(function (n) { a += n; })
+  Observable.fromArray(names)
+    .do(function (n) { a += n; })
     .filter(function (n) { return n % 2 === 0; })
-    .tap(function (n) { b += n; })
+    .do(function (n) { b += n; })
     .subscribe();
 
-  equal(__, a);
+  equal('123456', a);
   equal('246', b);
 });
 
 test('we wrote this', function () {
   var received = [],
-      names = ["Bart", "Marge", "Wes", "Linus", "Erik", "Matt"];
+    names = ["Bart", "Marge", "Wes", "Linus", "Erik", "Matt"];
 
-  Observable.from(names)
-    .filter(function (n) { return n.length <= __; })
+  Observable.fromArray(names)
+    .filter(function (n) { return n.length <= 4; })
     .subscribe(received.push.bind(received));
 
   equal('Bart,Wes,Erik,Matt', received);
@@ -44,10 +46,10 @@ test('we wrote this', function () {
 
 test('converting events', function () {
   var received = '',
-      names = ["wE", "hOpE", "yOU", "aRe", "eNJoyIng", "tHiS"];
+    names = ["wE", "hOpE", "yOU", "aRe", "eNJoyIng", "tHiS"];
 
-  Observable.from(names)
-    .map(function (x) { return x.__(); })
+  Observable.fromArray(names)
+    .map(function (x) { return x.toLowerCase(); })
     .subscribe(function (x) { received += x + ' '; });
 
   equal('we hope you are enjoying this ', received);
@@ -55,8 +57,10 @@ test('converting events', function () {
 
 test('create a more relevant stream', function () {
   var received = '',
-      mouseXMovements = [100, 200, 150],
-      relativemouse = Observable.from(mouseXMovements).map(function (x) { return x - __; });
+    mouseXMovements = [100, 200, 150],
+
+    relativemouse = Observable.fromArray(mouseXMovements)
+      .map(function (x) { return x - 50; });
 
   relativemouse.subscribe(function (x) { received += x + ', '; });
 
@@ -65,20 +69,20 @@ test('create a more relevant stream', function () {
 
 test('checking everything', function () {
   var received = null,
-      names = [2,4,6,8];
+    names = [2, 4, 6, 8];
 
-  Observable.from(names)
+  Observable.fromArray(names)
     .every(function (x) { return x % 2 === 0; })
     .subscribe(function (x) { received = x; });
 
-  equal(__, received);
+  equal(true, received);
 });
 
 test('composition means the sum is greater than the parts', function () {
   var received = 0,
-      numbers = Observable.range(1, 10);
+    numbers = Observable.range(1, 10);
 
-  numbers.filter(function (x) { return x > __; })
+  numbers.filter(function (x) { return x > 8; })
     .sum()
     .subscribe(function (x) { received = x; });
 
